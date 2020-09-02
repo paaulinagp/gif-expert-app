faefa32a8f6036f66d38fbf6b2dbfaf978ff1219
Paulina Guerrero <paaulinagp@gmail.com>
2020-09-01 21:30:05 -0500

    feat: creando componente de GifGridItem

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({category}) => {
+  const [images, setImages] = useState([]);  
+  
+  useEffect(() => {
+    getGifs();
+  }, []);
 
   const getGifs = async () => {
     const url =`https://api.giphy.com/v1/gifs/search?api_key=4BnvcbsxEOoVInUE8HhNpT7NxafDQy1V&q=Harry Potter`;
@@ -9,14 +15,19 @@ export const GifGrid = ({category}) => {
     const gifs = data.map( (gif) => ({
       id: gif.id,
       title: gif.title,
-      img: gif.images?.downsized_medium.url
+      url: gif.images?.downsized_medium.url
     }) );
-    console.log(gifs)
+    setImages(gifs);
   };
-  getGifs();
+
   return (
     <div>
       <h3>{category}</h3>
+        {
+          images.map( (img) => (
+            <GifGridItem key={img.id} {...img} />
+          ))
+        }
     </div>
   )
 }
