abfb38a133d3fb7be7cfdd867e8009194a1de058
Paulina Guerrero <paaulinagp@gmail.com>
2020-09-04 20:16:30 -0500

    feat: renombrando folder build por docs y agregando . en los src del index del build

---
+++ docs/precache-manifest.fdee64f94d9412e742732337de46228e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c071253099c96ca0fff204ec22f146e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d483e7ba602d1d93584",
+    "url": "/static/css/main.58b71974.chunk.css"
+  },
+  {
+    "revision": "a88b195f1dccd4342fee",
+    "url": "/static/js/2.d6db1626.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d6db1626.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d483e7ba602d1d93584",
+    "url": "/static/js/main.b9901e97.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
