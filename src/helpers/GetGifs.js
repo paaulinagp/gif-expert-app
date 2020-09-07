66a425f623dc9a89774ea134c494b405df6977e6
Paulina Guerrero <paaulinagp@gmail.com>
2020-09-07 13:45:37 -0500

    tests: agregando pruebas de helper GetGifs

--- src/helpers/GetGifs.js
+++ src/helpers/GetGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async (category) => {
-  const url =`https://api.giphy.com/v1/gifs/search?api_key=4BnvcbsxEOoVInUE8HhNpT7NxafDQy1V&q=${encodeURI(category)}`;
+  const url =`https://api.giphy.com/v1/gifs/search?api_key=4BnvcbsxEOoVInUE8HhNpT7NxafDQy1V&q=${encodeURI(category)}&limit=10`;
   const resp = await fetch(url);
   const { data } = await resp.json();
   const gifs = data.map( (gif) => ({
---
+++ src/tests/helpers/GetGifs.test.js
@@ -0,0 +1,14 @@
+import { getGifs } from '../../helpers/GetGifs';
+
+describe('Pruebas de helper GetGifs', () => {
+  describe('evaluando método getGifs', () => {
+    test('debe de traer 10 elementos', async() => {
+      const gifs = await getGifs('Harry Potter');
+      expect(gifs.length).toBe(10);
+    });
+    test('debe de traer 0 elementos', async() => {
+      const gifs = await getGifs('');
+      expect(gifs.length).toBe(0);
+    });    
+  });
+});
